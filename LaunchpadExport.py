48c2e99685a76f310bf9950bfebd5bb3bcfebeaa
Lukasz Piotrak <lukasz.piotrak@codilime.com>
2018-10-15 19:37:19 +0200

    Add update functionality (WIP)

--- LaunchpadExport.py
+++ LaunchpadExport.py
@@ -8,10 +8,10 @@ import sys
 from lp2jira.config import config
 
 
-def main(export_bugs=True, export_blueprints=True):
+def main(export_bugs=True, export_blueprints=True, update_bugs=False):
     from lp2jira.blueprint import ExportBlueprints
     from lp2jira.export import ExportCompile
-    from lp2jira.issue import ExportBugs
+    from lp2jira.issue import ExportBugs, UpdateBugs
     from lp2jira.user import ExportSubscribers
 
     logging.info('===== Export start =====')
@@ -20,9 +20,12 @@ def main(export_bugs=True, export_blueprints=True):
         ExportBugs().run()
     if export_blueprints:
         ExportBlueprints().run()
+
     logging.info('===== Compile export file =====')
     ExportCompile().run()
     logging.info('===== Export complete =====')
+    if update_bugs:
+        UpdateBugs().run()
 
 
 if __name__ == '__main__':
@@ -37,12 +40,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--only-bugs', help='Export only bugs', action='store_true')
     parser.add_argument('--only-blueprints', help='Export only blueprints', action='store_true')
+    parser.add_argument('--update-bugs', help='Update bugs', action='store_true')
     args = parser.parse_args()
 
     try:
         if args.only_bugs and args.only_blueprints:
             raise Exception('You can use only one of --only-bugs or --only-blueprints')
-        if args.only_bugs:
+        if args.update_bugs:
+            main(update_bugs=True)
+        elif args.only_bugs:
             main(export_blueprints=False)
         elif args.only_blueprints:
             main(export_bugs=False)
